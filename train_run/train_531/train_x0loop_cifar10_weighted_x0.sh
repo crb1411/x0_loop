40b6e040dee32def511772696e91e4ac70316be9
crb1411 <33300392+crb1411@users.noreply.github.com>
2026-05-31 09:30:28 +0800

    scripts: auto select torchrun master port for train531

--- train_run/train_531/train_x0loop_cifar10_weighted_x0.sh
+++ train_run/train_531/train_x0loop_cifar10_weighted_x0.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 cd /data/seek/aigc/x0_loop
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0}"
 export X0LOOP_RUN_TIMESTAMP="$(date +"%Y%m%d_%H%M%S")"
+
+MASTER_ADDR="${MASTER_ADDR:-127.0.0.1}"
+MASTER_PORT="${MASTER_PORT:-$(python - <<'PY'
+import socket
+s = socket.socket()
+s.bind(("", 0))
+print(s.getsockname()[1])
+s.close()
+PY
+)}"
+
+echo "[torchrun] MASTER_ADDR=${MASTER_ADDR} MASTER_PORT=${MASTER_PORT} CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
 
 RESUME_ARGS=""
 if [ -n "$1" ]; then
@@ -13,8 +25,8 @@ torchrun \
   --nnodes=1 \
   --node_rank=0 \
   --nproc_per_node=1 \
-  --master_addr=127.0.0.1 \
-  --master_port=40032 \
+  --master_addr="${MASTER_ADDR}" \
+  --master_port="${MASTER_PORT}" \
   -m x0loop.train \
   --config train_run/configs/cifar10/cifar10_dit_flow_train_x0_weighted.yaml \
   --runtime-config x0loop/configs/runtime/fsdp_checkpoint_compile.yaml \
